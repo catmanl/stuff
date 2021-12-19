9cf3d881b5c6f3bb0f67dc83a5bd820fcb016fab
laurenloco <laurenloco06@gmail.com>
2021-12-19 15:08:35 -0300

    Fix wrong build command

--- inventory/build.sh
+++ inventory/build.sh
@@ -1,1 +1,1 @@
-cc test.c -L ../common/raylib/lib/ -lraylib -lGL -lpthread -lm -ldl -I ../common/raylib/include/ -o main
+cc main.c -L ../common/raylib/lib/ -lraylib -lGL -lpthread -lm -ldl -I ../common/raylib/include/ -o main
